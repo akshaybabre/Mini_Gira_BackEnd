5d007efe962d9000fd6684022dd44e01757170b7
akshaybabre <akshaybabre8@gmail.com>
2026-02-11 23:02:13 +0530

    Adding

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: true, 
     credentials: true,     
   }));
 app.use(express.json());
